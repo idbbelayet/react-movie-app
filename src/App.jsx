0b6a42acbfa593b2f4c103b6a8459d589c47a469
belayetsci <alnoorkg@gmail.com>
2025-04-21 14:15:28 +0600

    app js modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useEffect, useState } from 'react';
+import './App.css';
+const Card = ({ title, description }) => {
+  const [count,setCount] =useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+  const handleLike = () => {
+    setHasLiked(!hasLiked);
+    setCount(count + 1);
+  }
+ 
+  useEffect(() => {
+     console.log(`${title} has been ${hasLiked ? 'liked' : 'unliked'}`);
+  },[hasLiked]);
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card" >
+      <h2>{title}  </h2>
+      <p>{description}</p>
+      <button onClick={handleLike}>{hasLiked ? 'Liked' : 'Like'} {count}</button>
+    </div>
+  )
+}
+const App = () => {
+  return (
+    <div className="card-container">
+       
+      <Card title="Card 1" description="description 1" />
+      <Card title="Card 2" description="description 2" />
+      <Card title="Card 3" description="description 3" />
+    </div>
   )
 }
 
